5b635b2719d59f2ba3fc2b29508121b26e89ef56
Vítor Marçal <git@vitormarcal.com.br>
2020-11-07 08:17:51 -0300

    add carga para especialidade

--- src/main/java/br/com/sismedicina/gestor/model/Especialidade.java
+++ src/main/java/br/com/sismedicina/gestor/model/Especialidade.java
@@ -1,12 +1,15 @@
 package br.com.sismedicina.gestor.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Especialidade {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String descricao;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_TECNICO');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde da mulher')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde mental')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Primeiro socorros')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Covid-19')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde do idoso')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde do adulto')
+INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde da criança e adolescente')
