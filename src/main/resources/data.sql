af07fe818e67920339c9ae7a5aba235bdc99dfc3
Vítor Marçal <git@vitormarcal.com.br>
2020-11-07 08:53:14 -0300

    add carga para especialidade

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,10 @@
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_TECNICO');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
-
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde da mulher')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde mental')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Primeiro socorros')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Covid-19')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde do idoso')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde do adulto')
-INSERT INTO ESPECIALIDADE(descricao) VALUES('Saúde da criança e adolescente')
+INSERT INTO especialidade(descricao) VALUES('Saúde da mulher');
+INSERT INTO especialidade(descricao) VALUES('Saúde mental');
+INSERT INTO especialidade(descricao) VALUES('Primeiro socorros');
+INSERT INTO especialidade(descricao) VALUES('Covid-19');
+INSERT INTO especialidade(descricao) VALUES('Saúde do idoso');
+INSERT INTO especialidade(descricao) VALUES('Saúde do adulto');
+INSERT INTO especialidade(descricao) VALUES('Saúde da criança e adolescente');
