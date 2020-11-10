41d8e748fe88a5c233988d9f3ac57a5606f05f8a
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-10 16:32:11 -0300

    atualização do tecnico

--- src/main/java/br/com/sismedicina/gestor/controller/ConsultaController.java
+++ src/main/java/br/com/sismedicina/gestor/controller/ConsultaController.java
@@ -1,14 +1,17 @@
 package br.com.sismedicina.gestor.controller;
 
+import br.com.sismedicina.gestor.model.Consulta;
 import br.com.sismedicina.gestor.payload.request.FiltroConsultaDisponivelRequest;
 import br.com.sismedicina.gestor.payload.response.ConsultaDisponivelResponse;
+import br.com.sismedicina.gestor.security.services.UserDetailsImpl;
 import br.com.sismedicina.gestor.services.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("consultas")
@@ -22,4 +25,12 @@ public class ConsultaController {
     public List<ConsultaDisponivelResponse> buscarConsultasDisponiveis(FiltroConsultaDisponivelRequest filtro) {
         return consultaService.buscarAgendasDisponiveis(filtro);
     }
+
+    @PutMapping("/disponiveis/{idConsulta}")
+    @PreAuthorize("hasRole('USER')")
+    public Optional<Consulta> buscarConsultasDisponiveis(@PathVariable Long idConsulta, UsernamePasswordAuthenticationToken userDetails) {
+        UserDetailsImpl principal = (UserDetailsImpl) userDetails.getPrincipal();
+        return consultaService.agendarParaEsteUsuario(idConsulta, principal);
+    }
+
 }
